4df1849278ae87b65a6962f0bcbdf2ba380270b1
nevalyaprak <147134515+nevalyaprak@users.noreply.github.com>
2025-06-06 00:17:56 +0300

    Create TokenC.sol

---
+++ contracts/TokenC.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.27;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract TokenA is ERC20, Ownable, ERC20Permit {
+    constructor(address recipient, address initialOwner)
+        ERC20("Token C", "TKC")
+        Ownable(initialOwner)
+        ERC20Permit("Token C")
+    {
+        _mint(recipient, 1000000 * 10 ** decimals());
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
