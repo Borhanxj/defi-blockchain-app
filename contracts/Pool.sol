c1b7034aa072b639d32d00bbc92d733bbcfd35f3
nevalyaprak <147134515+nevalyaprak@users.noreply.github.com>
2025-06-05 14:45:10 +0300

    Fixed naming

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -4,12 +4,12 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Pool {
-    address public tokenA;
-    address public tokenB;
+    address public token0;
+    address public token1;
     address public owner;
 
-    uint256 public liquidityA;
-    uint256 public liquidityB;
+    uint256 public liquidity0;
+    uint256 public liquidity1;
 
     mapping(address => uint256) public lpShares;
     uint256 public totalShares;
@@ -18,28 +18,28 @@ contract Pool {
     uint256 public constant FEE_DENOMINATOR = 1000;
 
     constructor(
-        address _tokenA,
-        address _tokenB,
+        address _token0,
+        address _token1,
         address _creator
     ) {
-        require(_tokenA != _tokenB, "Same token");
-        tokenA = _tokenA;
-        tokenB = _tokenB;
+        require(_token0 != _token1, "Same token");
+        tokenA = _token0;
+        tokenB = _token1;
         owner = _creator;
     }
-    function initializeLiquidity(uint256 amountA, uint256 amountB) external {
-        require(liquidityA == 0 && liquidityB == 0, "Already initialized");
-        liquidityA = amountA;
-        liquidityB = amountB;
+    function initializeLiquidity(uint256 amount0, uint256 amount1) external {
+        require(liquidity0 == 0 && liquidity1 == 0, "Already initialized");
+        liquidity0 = amount0;
+        liquidity1 = amount1;
     }
 
 
     function swap(address tokenIn, uint256 amountIn, uint256 minAmountOut) external {
-        require(tokenIn == tokenA || tokenIn == tokenB, "Invalid tokenIn");
-        address tokenOut = tokenIn == tokenA ? tokenB : tokenA;
+        require(tokenIn == token0 || tokenIn == token1, "Invalid tokenIn");
+        address tokenOut = tokenIn == token0 ? token1 : token0;
 
-        uint256 liquidityIn = tokenIn == tokenA ? liquidityA : liquidityB;
-        uint256 liquidityOut = tokenIn == tokenA ? liquidityB : liquidityA;
+        uint256 liquidityIn = tokenIn == token0 ? liquidity0 : liquidity1;
+        uint256 liquidityOut = tokenIn == token0 ? liquidity1 : liquidity0;
 
         uint256 amountInWithFee = (amountIn * FEE_NUMERATOR) / FEE_DENOMINATOR;
         uint256 amountOut = (liquidityOut * amountInWithFee) / (liquidityIn + amountInWithFee);
@@ -50,31 +50,31 @@ contract Pool {
         IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
         IERC20(tokenOut).transfer(msg.sender, amountOut);
 
-        if (tokenIn == tokenA) {
-            liquidityA += amountIn;
-            liquidityB -= amountOut;
+        if (tokenIn == token0) {
+            liquidity0 += amountIn;
+            liquidity1 -= amountOut;
         } else {
-            liquidityB += amountIn;
-            liquidityA -= amountOut;
+            liquidity1 += amountIn;
+            liquidity0 -= amountOut;
         }
     }
 
 
-    function addLiquidity(uint256 amountA, uint256 amountB) external {
-        require( liquidityA * amountB == liquidityB * amountA,"Deposit ratio mismatch");
+    function addLiquidity(uint256 amount0, uint256 amount1) external {
+        require( liquidity0 * amount1 == liquidity1 * amount0,"Deposit ratio mismatch");
 
-        IERC20(tokenA).transferFrom(msg.sender, address(this), amountA);
-        IERC20(tokenB).transferFrom(msg.sender, address(this), amountB);
+        IERC20(token0).transferFrom(msg.sender, address(this), amount0);
+        IERC20(token1).transferFrom(msg.sender, address(this), amount1);
 
-        liquidityA += amountA;
-        liquidityB += amountB;
+        liquidity0 += amount0;
+        liquidity1 += amount1;
 
         uint256 share;
         if (totalShares == 0) {
-            share = amountA + amountB;
+            share = amount0 + amount1;
         }
         else {
-            share = ((amountA + amountB) * totalShares) / (liquidityA + liquidityB);
+            share = ((amount0 + amount1) * totalShares) / (liquidity0 + liquidity1);
         }
 
         lpShares[msg.sender] += share;
@@ -85,24 +85,24 @@ contract Pool {
         require(share > 0, "Zero share");
         require(lpShares[msg.sender] >= share, "Not enough share");
 
-        uint256 amountA = (liquidityA * share) / totalShares;
-        uint256 amountB = (liquidityB * share) / totalShares;
+        uint256 amount0 = (liquidity0 * share) / totalShares;
+        uint256 amount1 = (liquidity1 * share) / totalShares;
 
         lpShares[msg.sender] -= share;
         totalShares -= share;
 
-        liquidityA -= amountA;
-        liquidityB -= amountB;
+        liquidity0 -= amount0;
+        liquidity1 -= amount1;
 
-        IERC20(tokenA).transfer(msg.sender, amountA);
-        IERC20(tokenB).transfer(msg.sender, amountB);
+        IERC20(tokenA).transfer(msg.sender, amount0);
+        IERC20(tokenB).transfer(msg.sender, amount1);
     }
 
     function addLiquiditySingleToken(address tokenIn, uint256 amountIn) external {
-        require(tokenIn == tokenA || tokenIn == tokenB, "Invalid token");
+        require(tokenIn == token0 || tokenIn == token1, "Invalid token");
 
-        uint256 liquidityIn = tokenIn == tokenA ? liquidityA : liquidityB;
-        uint256 liquidityOut = tokenIn == tokenA ? liquidityB : liquidityA;
+        uint256 liquidityIn = tokenIn == token0 ? liquidity0 : liquidity1;
+        uint256 liquidityOut = tokenIn == token0 ? liquidity1 : liquidity0;
 
         IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
 
@@ -111,17 +111,17 @@ contract Pool {
 
         require(amountOut <= liquidityOut, "Not enough liquidity to rebalance");
 
-        if (tokenIn == tokenA) {
-            liquidityA += amountIn;
-            liquidityB -= amountOut;
+        if (tokenIn == token0) {
+            liquidity0 += amountIn;
+            liquidity1 -= amountOut;
         } 
         else {
-        liquidityB += amountIn;
-        liquidityA -= amountOut;
+        liquidity1 += amountIn;
+        liquidity0 -= amountOut;
         }
 
         uint256 liquidityAdded = amountIn + amountOut;
-        uint256 totalLiquidity = liquidityA + liquidityB;
+        uint256 totalLiquidity = liquidity0 + liquidity1;
         uint256 share = (liquidityAdded * totalShares) / totalLiquidity;
 
         lpShares[msg.sender] += share;
