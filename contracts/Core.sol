1b52fead2379ee1849f198747f53e0572ddf17a7
nevalyaprak <147134515+nevalyaprak@users.noreply.github.com>
2025-06-08 16:18:07 +0300

    event logs

--- contracts/Core.sol
+++ contracts/Core.sol
@@ -2,15 +2,30 @@
 pragma solidity ^0.8.20;
 
 import "./AMM.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Core is ReentrancyGuard {
-
     using SafeERC20 for IERC20;
 
     address[] public allPools;
 
-    function createPool(address token0,address token1,uint256 amount0, uint256 amount1) external nonReentrant returns (address) {
+    event PoolCreated(
+        address indexed creator,
+        address indexed token0,
+        address indexed token1,
+        address poolAddress,
+        uint256 amount0,
+        uint256 amount1,
+        uint256 timestamp
+    );
 
+    function createPool(
+        address token0,
+        address token1,
+        uint256 amount0,
+        uint256 amount1
+    ) external nonReentrant returns (address) {
         IERC20(token0).safeTransferFrom(msg.sender, address(this), amount0);
         IERC20(token1).safeTransferFrom(msg.sender, address(this), amount1);
 
@@ -21,6 +36,16 @@ contract Core is ReentrancyGuard {
 
         newPool.initializeLiquidity(amount0, amount1);
         allPools.push(address(newPool));
+
+        emit PoolCreated(
+            msg.sender,
+            token0,
+            token1,
+            address(newPool),
+            amount0,
+            amount1,
+            block.timestamp
+        );
 
         return address(newPool);
     }
